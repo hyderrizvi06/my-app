c88dbefc4e00df84d763481c73984a1fcdd187f7
Hyder Rizvi <hmrizvi@stu.naperville203.org>
2023-12-04 12:40:30 -0600

    Finished REACT

---
+++ src/test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+class Test extends React.Component{
+    render() {
+        return(
+            <div className="Test">
+                <h1>
+                    Test code
+                </h1>
+                <ul>
+                    <li> item 1</li>
+                    <li> item 2</li>
+                    <li> item 3</li>
+                </ul>
+
+                <a href="https://naperville.instructure.com/courses/1168978/assignments/6647614">Link to canvas</a>
+            
+                
+            </div>
+        )
+    }
+}
+
+export default Test;
